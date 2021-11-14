ce3e8ec6c11626aeadc5175c162b8fc578ac1c12
brandon2278 <92067718+brandon2278@users.noreply.github.com>
2021-11-14 18:40:15 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var home = document.getElementById("home");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+home.onload = function () {
+  modal.style.display = "block";
+}
+
+var translater = document.getElementById("translater");
+
+// Get the button that opens the modal
+var btn = document.getElementById("translate");
+var answer = document.getElementById("subm");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  translater.style.display = "block";
+}
+answer.onclick = function () {
+  translater.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    translater.style.display = "none";
+  }
+}
+
+
